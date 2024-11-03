2eaf87933eb3157c5331499cb62ab5b8eae66491
plaha303 <111688671+plaha303@users.noreply.github.com>
2024-11-03 02:40:37 +0200

    add heroku postgresql

--- fincontrol/settings.py
+++ fincontrol/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 import os
 import django_heroku
 
@@ -68,13 +69,14 @@ WSGI_APPLICATION = "fincontrol.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_USER_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-    }
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": os.getenv("DB_NAME"),
+    #     "USER": os.getenv("DB_USER"),
+    #     "PASSWORD": os.getenv("DB_USER_PASSWORD"),
+    #     "HOST": os.getenv("DB_HOST"),
+    # }
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
